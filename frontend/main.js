8f8786917cb8a9cc6b645832472a62dd32e10bbe
swetha <staringswetha@gmail.com>
2025-04-09 15:34:49 +0530

    created the frontend

---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const productionApiUrl = 'https://gpsazureresumecounter.azurewebsites.net/api/GetVisitorCounter?';
+const localApiUrl = 'http://localhost:7071/api/GetVisitorCounter';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(productionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count =  response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
